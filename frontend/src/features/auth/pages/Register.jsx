d3a315719c286f6db1d7eac56b4872dfa15c510b
Jhonny Castro <Jhocastrol.99@gmail.com>
2024-08-20 00:57:14 -0500

    redirect register to /

--- frontend/src/features/auth/pages/Register.jsx
+++ frontend/src/features/auth/pages/Register.jsx
@@ -1,14 +1,25 @@
-
-import React from 'react';
-import AuthTemplate from '../components/templates/AuthTemplate';
-import RegisterForm from '../components/forms/RegisterForm';
+import React, { useEffect } from "react";
+import AuthTemplate from "../components/templates/AuthTemplate";
+import RegisterForm from "../components/forms/RegisterForm";
 import "../styles/auth.css";
-import 'react-toastify/dist/ReactToastify.css';
+import "react-toastify/dist/ReactToastify.css";
+import { useAuth } from "../context/AuthContext";
+import { useNavigate } from "react-router-dom";
 
-const Register= () => (
-  <AuthTemplate title={"Register"}>
-    <RegisterForm />
-  </AuthTemplate>
-);
+const Register = () => {
+  const { authState } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (authState.token) {
+      navigate("/");
+    }
+  }, [authState, navigate]);
+  return (
+    <AuthTemplate title={"Register"}>
+      <RegisterForm />
+    </AuthTemplate>
+  );
+};
 
 export default Register;
